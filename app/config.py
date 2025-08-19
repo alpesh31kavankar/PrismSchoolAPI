fffad1072b64efb99140d769fd5116addce7af6b
Alpesh <alpesh31kavankar@gmail.com>
2025-08-19 12:56:04 +0530

    Use DATABASE_URL from env with local fallback

--- app/config.py
+++ app/config.py
@@ -1,4 +1,10 @@
 # DATABASE_URL = "postgresql://postgres:2021Prismit$@localhost:5432/PrismSchoolDB"
 
 
-DATABASE_URL = "postgresql://prism:GyXzhosyeNZfI6ffPCXuHajXqoTLOJhe@dpg-d2i153be5dus73e8u17g-a.oregon-postgres.render.com:5432/prismschooldb"
+# DATABASE_URL = "postgresql://prism:GyXzhosyeNZfI6ffPCXuHajXqoTLOJhe@dpg-d2i153be5dus73e8u17g-a.oregon-postgres.render.com:5432/prismschooldb"
+import os
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:2021Prismit$@localhost:5432/PrismSchoolDB"  # fallback for local
+)
